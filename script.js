fb89903754dddde1e42e625ec15362297c82b919
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-28 15:44:25 +0200

    ajout commentaires

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//EX 1
+//---EX 1---
 //  Add a keyup listener on the first input field, so that once you type a letter in this field, it goes into the <span id="display-firstname">. The content of the field and the span should always remain the same.
 
 // on choppe l'input et le span via leur ID
@@ -12,7 +12,7 @@ inputField.addEventListener("keyup", function () {
   console.log(readyToDisplay);
 });
 
-//EX2
+//---EX2---
 // Add a keyup listener on the second input (the number input), so that if the age is below 18 the content of the section a-hard-truth remains hidden, otherwise show them this hard to swallow fact.
 
 // si age <18 --> section reste cachée
@@ -26,7 +26,7 @@ ageField.addEventListener("keyup", function () {
   } else sectionAge.style.visibility = "visible";
 });
 
-//EX3
+//---EX3---
 //Well this is a common one. Add a keyup listener on both fields and show a visual hint (for instance turn the field red) if the password is too short (less than 6 characters) or if the password and its confirmation do not match.
 
 // si le mdp fait moins de 6 caractère --> border rouge
@@ -51,19 +51,18 @@ mdpConfirm.addEventListener("keyup", function () {
   } else mdpConfirm.style.border = "2px solid red";
 });
 
-//EX4
+//---EX4---
 //Finally, use a change listener on the <select> field to toggle a dark mode on the whole page. For ease of use, we'll say that the dark mode is just turning the background black and the text white.
 
 let darkmodeSelector = document.querySelector("#toggle-darkmode");
 darkmodeSelector.addEventListener("change", function () {
   let darkmodeValue = darkmodeSelector.value;
-  let body = document.body;
 
   if (darkmodeValue === "dark") {
-    body.style.backgroundColor = "black";
-    body.style.color = "white";
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
   } else {
-    body.style.backgroundColor = "white";
-    body.style.color = "black";
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
   }
 });
